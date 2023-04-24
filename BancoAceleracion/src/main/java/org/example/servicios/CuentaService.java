0cb1f25787594a783a2cb141494dc5e67265a721
Maxi <maxi.ush.ind@gmail.com>
2023-04-24 10:05:54 -0300

    Modificaciones sist bancario 3

--- BancoAceleracion/src/main/java/org/example/servicios/CuentaService.java
+++ BancoAceleracion/src/main/java/org/example/servicios/CuentaService.java
@@ -14,28 +14,30 @@ public class CuentaService {
     public void depositar(Cuenta cuenta, double monto){
 
         mostrarSaldo(cuenta);
+
         cuenta.setSaldo(cuenta.getSaldo() + monto);
         System.out.println("Se deposito correctamente $" + monto);
+
         mostrarSaldo(cuenta);
     }
 
     public void retirar(Cuenta cuenta, double monto){
 
         mostrarSaldo(cuenta);
+
         if(validarSaldo(cuenta, monto)){
             cuenta.setSaldo(cuenta.getSaldo() - monto);
             System.out.println("Se extrajo correctamente $" + monto);
         } else {
             System.out.println("Quiso retirar $" + monto + " Y el saldo es insuficiente para realizar la operacion");
         }
+
         mostrarSaldo(cuenta);
     }
 
     public void transferir(Cuenta cuentaOrigen, Cuenta cuentaDestino, double monto){
-        System.out.println("--Cuenta Origen--");
-        mostrarSaldo(cuentaOrigen);
-        System.out.println("--Cuenta Destino--");
-        mostrarSaldo(cuentaDestino);
+
+        mostrarSaldos(cuentaOrigen, cuentaDestino);
 
         if(validarSaldo(cuentaOrigen, monto)){
             cuentaOrigen.setSaldo(cuentaOrigen.getSaldo() - monto); // descontarSaldo()
@@ -45,10 +47,7 @@ public class CuentaService {
             System.out.println("Se quiso transferir $" + monto + " y el saldo es insuficiente para realizar la operacion");
         }
 
-        System.out.println("--Cuenta Origen--");
-        mostrarSaldo(cuentaOrigen);
-        System.out.println("--Cuenta Destino--");
-        mostrarSaldo(cuentaDestino);
+        mostrarSaldos(cuentaOrigen, cuentaDestino);
     }
 
     public boolean validarSaldo(Cuenta cuenta, double monto){
@@ -58,5 +57,13 @@ public class CuentaService {
 
     public void mostrarSaldo(Cuenta cuenta){
         System.out.println("------ Saldo actual: $" + cuenta.getSaldo() + " ------");
+    }
+
+    public void mostrarSaldos(Cuenta cuentaOrigen, Cuenta cuentaDestino){
+
+        System.out.println("--Cuenta Origen--");
+        mostrarSaldo(cuentaOrigen);
+        System.out.println("--Cuenta Destino--");
+        mostrarSaldo(cuentaDestino);
     }
 }
